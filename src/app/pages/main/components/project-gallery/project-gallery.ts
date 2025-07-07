55f8a5074c82374190c1a1efc62ecc38a6f80007
Royer Adames <royeraadames@gmail.com>
2025-07-07 19:04:39 -0400

    remove main navigation component and associated styles and data

---
+++ src/app/pages/main/components/project-gallery/project-gallery.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import projectGallery from '../../../../shared/data/project-gallery.data';
+
+@Component({
+  selector: 'app-project-gallery',
+  templateUrl: './project-gallery.html',
+  styleUrl: './project-gallery.css',
+  imports: [
+    // MainNavigation,
+    RouterLink,
+    NgFor,
+  ],
+})
+export class ProjectGallery {
+  public ProjectGallery = projectGallery;
+}
