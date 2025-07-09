a111ce487faa93fa428d70b253847bc6a3c02ed0
Royer Adames <royeraadames@gmail.com>
2025-07-09 19:35:50 -0400

    Add new SVG icons for Apollo GraphQL, Cypress, Jest, and RESTful API; update skilled-with component to use new icon filenames and image component

--- src/app/pages/main/components/resume/components/skilled-with/skilled-with.ts
+++ src/app/pages/main/components/resume/components/skilled-with/skilled-with.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import skilledWithData from '@shared/data/resume/skilled-with.data';
+import { Image } from "../../../../../../shared/components/image/image";
 
 @Component({
   selector: 'app-skilled-with',
   templateUrl: './skilled-with.html',
   styleUrl: './skilled-with.css',
-  imports: [],
+  imports: [Image],
 })
 export class SkilledWith {
   public skilledWith = skilledWithData;
