f3ddc098d3d48be85c828016f097c42c7c398a2b
Royer Adames <royeraadames@gmail.com>
2025-07-10 16:09:16 -0400

    missing project images, disable for now

--- src/app/pages/main/components/resume/components/projects/projects.ts
+++ src/app/pages/main/components/resume/components/projects/projects.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import projectsData from '@shared/data/resume/projects.data';
+import { Image } from "../../../../../../shared/components/image/image";
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.html',
   styleUrl: './projects.css',
-  imports: [],
+  imports: [Image],
 })
 export class Projects {
   public projects = projectsData;
--- src/app/shared/data/resume/projects.data.ts
+++ src/app/shared/data/resume/projects.data.ts
@@ -1,35 +1,43 @@
 const projects = [
   {
-    image: "universal-orlando.webp",
-    title: "UNIVERSAL STUDIO ORLANDO WEBSITE",
-    link: "https://www.universalorlando.com/web/en/us",
+    image: {
+      name: 'universal-orlando.webp',
+      height: 100,
+      width: 100,
+    },
+    title: 'UNIVERSAL STUDIO ORLANDO WEBSITE',
+    link: 'https://www.universalorlando.com/web/en/us',
     messageList: [
       {
-        title: "Web Accessibility Improvement",
+        title: 'Web Accessibility Improvement',
         description:
-          "Engaged in significant enhancements to the universalstudios.com website, ensuring improved web accessibility and user experience.",
+          'Engaged in significant enhancements to the universalstudios.com website, ensuring improved web accessibility and user experience.',
       },
       {
-        title: "Angular-Based UI Development",
+        title: 'Angular-Based UI Development',
         description:
-          "Contributed to the development of Angular-based UI features, adhering to tight deadlines and supporting digital data analytics.",
+          'Contributed to the development of Angular-based UI features, adhering to tight deadlines and supporting digital data analytics.',
       },
     ],
   },
   {
-    image: "mitsubishi-contract-risk-management.webp",
-    title: "MITSUBISHI CONTRACT RISK MANAGEMENT PLATFORM",
-    link: "",
+    image: {
+      name: 'mitsubishi-contract-risk-management.webp',
+      height: 100,
+      width: 100,
+    },
+    title: 'MITSUBISHI CONTRACT RISK MANAGEMENT PLATFORM',
+    link: '',
     messageList: [
       {
-        title: "Responsive UI Development",
+        title: 'Responsive UI Development',
         description:
           "Developed a responsive UI, ensuring a consistent and optimal user experience across various devices in a platform for Mitsubishi's Contract Risk Management.",
       },
       {
-        title: "System Security",
+        title: 'System Security',
         description:
-          "Established a robust system for app security and access management, protecting sensitive data, and controlling user access effectively.",
+          'Established a robust system for app security and access management, protecting sensitive data, and controlling user access effectively.',
       },
     ],
   },
