6404b9d45a6173a4900ccf9744558548d8175b27
Royer Adames <royeraadames@gmail.com>
2025-07-10 16:01:04 -0400

    companies logo

--- src/app/pages/main/components/resume/components/experience/experience.ts
+++ src/app/pages/main/components/resume/components/experience/experience.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import experienceData from '@shared/data/resume/experience.data';
+import { Image } from "../../../../../../shared/components/image/image";
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.html',
   styleUrl: './experience.css',
-  imports: [],
+  imports: [Image],
 })
 export class Experience {
   public experience = experienceData;
