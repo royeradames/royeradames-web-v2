fb6cdb6c9d2a9dd2deaa950a405120e3cd8d585a
Royer Adames <royeraadames@gmail.com>
2025-07-10 16:17:16 -0400

    all visuals are now loading

--- src/app/pages/main/components/resume/components/education/education.ts
+++ src/app/pages/main/components/resume/components/education/education.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import educationData from '@shared/data/resume/education.data';
+import { Image } from "../../../../../../shared/components/image/image";
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.html',
   styleUrl: './education.css',
-  imports: [],
+  imports: [Image],
 })
 export class Education {
   public education = educationData;
