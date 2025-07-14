7d7d2e958d60bdedb828cd24f87b4717fe9fef06
Royer Adames <royeraadames@gmail.com>
2025-07-13 22:05:09 -0400

    Refactor global styles by removing the modern CSS reset and importing a separate reset file. Update education component styles to simplify gap settings and clean up unused flex properties. Comment out unused imports in the projects component for clarity.

--- src/app/pages/main/components/resume/components/projects/projects.ts
+++ src/app/pages/main/components/resume/components/projects/projects.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import projectsData from '@shared/data/resume/projects.data';
-import { Image } from '@shared/components/image/image';
+// import { Image } from '@shared/components/image/image';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.html',
   styleUrl: './projects.css',
-  imports: [Image],
+  // imports: [Image],
 })
 export class Projects {
   public projects = projectsData;
