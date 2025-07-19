40de056951a517f572c4201d319a11cd7cb18e63
Royer Adames <royeraadames@gmail.com>
2025-07-18 23:31:36 -0400

    Refactor projects component by updating HTML structure and CSS styles for improved clarity and maintainability. Introduce new class names and layout adjustments, enhancing the overall design and user experience.

--- src/app/pages/main/components/resume/components/projects/projects.ts
+++ src/app/pages/main/components/resume/components/projects/projects.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import projectsData from '@shared/data/resume/projects.data';
+import { Image } from "@shared/components/image/image";
 // import { Image } from '@shared/components/image/image';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.html',
   styleUrl: './projects.css',
+  imports: [Image],
   // imports: [Image],
 })
 export class Projects {
