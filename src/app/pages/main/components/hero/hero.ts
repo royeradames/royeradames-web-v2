a44679dd6a9615b8ed46852c66170186e641e259
Royer Adames <royeraadames@gmail.com>
2025-07-26 19:36:04 -0400

    Enhance main page layout and navigation structure
    - Introduce a new header section in the main page with a navigation bar for improved accessibility and organization.
    - Add styles for the hero navigation to ensure proper alignment and spacing.
    - Refactor hero component to streamline the HTML structure and integrate new navigation elements, enhancing user experience and maintainability.

--- src/app/pages/main/components/hero/hero.ts
+++ src/app/pages/main/components/hero/hero.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import headerData from '@shared/data/header.data';
-import { SocialMediaLinks } from '@shared/components/social-media-links/social-media-links';
 import { Image } from '@shared/components/image/image';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -10,20 +9,17 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavigationLinks } from './navigation-links/navigation-links';
 import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MobileMenuButton } from '../mobile-menu/mobile-menu-button/mobile-menu-button';
-import { RouterLink } from '@angular/router';
 import footerData from '@shared/data/footer.data';
-import { Positions } from "./positions/positions";
-import { Message } from "./message/message";
-import { SkillList } from "./skill-list/skill-list";
-import { Contact } from "./contact/contact";
+import { Positions } from './positions/positions';
+import { Message } from './message/message';
+import { SkillList } from './skill-list/skill-list';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.html',
   styleUrl: './hero.css',
   imports: [
-    SocialMediaLinks,
     Image,
     MatButtonModule,
     MatIconModule,
@@ -34,13 +30,11 @@ import { Contact } from "./contact/contact";
     MatRadioModule,
     FormsModule,
     ReactiveFormsModule,
-    MobileMenuButton,
-    RouterLink,
     Positions,
     Message,
     SkillList,
-    Contact
-],
+    Contact,
+  ],
   standalone: true,
 })
 export class Hero {
--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -4,11 +4,49 @@ import { Hero } from './components/hero/hero';
 import { ProjectGallery } from './components/project-gallery/project-gallery';
 import { Resume } from './components/resume/resume';
 import { Footer } from './components/footer/footer';
+import { SocialMediaLinks } from '@shared/components/social-media-links/social-media-links';
+import { MobileMenuButton } from './components/mobile-menu/mobile-menu-button/mobile-menu-button';
+import { NavigationLinks } from './components/hero/navigation-links/navigation-links';
+import { Image } from '@shared/components/image/image';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import headerData from '@shared/data/header.data';
 
 @Component({
   selector: 'page-main',
   templateUrl: './main.page.html',
   styleUrl: './main.page.css',
-  imports: [Hero, ProjectGallery, Resume, Footer, MobileMenu],
+  imports: [
+    Hero,
+    ProjectGallery,
+    Resume,
+    Footer,
+    MobileMenu,
+    SocialMediaLinks,
+    MobileMenuButton,
+    NavigationLinks,
+    Image,
+    SocialMediaLinks,
+    Image,
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatSidenavModule,
+    NavigationLinks,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MobileMenuButton,
+    RouterLink,
+  ],
 })
-export class MainPage {}
+export class MainPage {
+  public headerData = headerData;
+}
