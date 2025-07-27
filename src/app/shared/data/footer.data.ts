c12873ff049f27a6efbf33afc71547498c8697e1
Royer Adames <royeraadames@gmail.com>
2025-07-26 21:31:29 -0400

    Refactor footer component styles and structure
    - Update footer styles to enhance layout and visual appeal, including new flex properties and background image.
    - Modify HTML structure to group call-to-action elements, improving organization and accessibility.
    - Simplify footer data content for clarity and conciseness.

--- src/app/shared/data/footer.data.ts
+++ src/app/shared/data/footer.data.ts
@@ -2,10 +2,8 @@ import resumeAnchorLinks from './resume-anchor-links.data';
 
 const footerData = {
   callToAction: {
-    title: "Let's build something together",
-    message:
-      "I'm currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I'll try my best to get back to you!",
-    contactMessage: 'Drop me an email:',
+    title: "LET'S BUILD TOGETHER",
+    message: 'Drop me an email:',
     contactEmail: 'CONTACT@ROYERADAMES.COM',
   },
   resumeLinks: [
